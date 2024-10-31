c735619ac9c99d38150131fd5aa9793f4b35efff
bp3000bp <jculbertsonmp@gmail.com>
2024-10-31 14:42:54 -0500

    after PART 4

--- flask_blog_class/app.py
+++ flask_blog_class/app.py
@@ -4,6 +4,7 @@ from flask import Flask, render_template, request, url_for, flash, redirect, abo
 # make a Flask application object called app
 app = Flask(__name__)
 app.config["DEBUG"] = True
+app.config['SECRET_KEY'] = 'your secret key'
 
 
 
@@ -37,6 +38,29 @@ def index():
 
 
 # route to create a post
+@app.route('/create/', methods=('GET', 'POST'))
+def create():
+    #determind if the page isbeing requested w a post or get request
+    if request.method == 'POST':
+        #get the title and content that swas submitted
+        title = request.form['title']
+        content = request.form['content']
 
+        #display an error message if title or content is not submitted
+        #else make a db connection and insert the blog post content
+        if not title:
+            flash("Title is required")
+        elif not content:
+            flash("Content is required")
+        else:
+            conn = get_db_connection()
+            #insert data into db
+            conn.execute('INSERT INTO posts (title, content) VALUES (?, ?)', (title,content))
+            conn.commit()
+            conn.close()
+            return redirect(url_for('index'))
+
+
+    return render_template('create.html')
 
 app.run()
